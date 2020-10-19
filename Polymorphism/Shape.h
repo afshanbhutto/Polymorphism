a1fbae89c9de51a61aea7488a8065f49c9d89403
Ali Murtaza <58934980+Ali-Murtaza143@users.noreply.github.com>
2020-10-19 22:57:47 +0500

    adding functions

--- Polymorphism/Shape.h
+++ Polymorphism/Shape.h
@@ -9,6 +9,8 @@ class Shape
         virtual void introduce() = 0;
         virtual double calculateArea() = 0;
         virtual double calculateCircumference() = 0;
+    long double calculateShapeArea(Shape*);
+long double calculateShapeCircumferece(Shape*);
 };
 
 #endif // SHAPE_H_INCLUDED
